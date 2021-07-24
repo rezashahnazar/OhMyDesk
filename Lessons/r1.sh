806e9685d8341e93b634f9f5655575738fd4da49
Reza <reza.shahnazar@gmail.com>
2021-07-24 21:28:59 +0430

    edditing l1

--- Lessons/r1.sh
+++ Lessons/r1.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+# This lesson is about $ sign and its 2 main uses.
+# $X is for accessing the data stored in the X variable
+# $(command) is for accessing the output of a command
+# Just hear that have some environment variables like $HOME $PATH $PS1. we don't go into the details of environments.
+
 DESKTOP="$HOME/Desktop"
 DEF_NAME="OhMyDesk"
 TIME=$(date | awk '{print $4}')
